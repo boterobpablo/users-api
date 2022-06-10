600968a76db7e783a09f2737b16186f717de6589
Juan Pablo Botero <boterobpablo@utp.edu.co>
2022-06-10 16:55:29 -0500

    name project changed

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/api-jsonplaceholder'),
+      dir: require('path').join(__dirname, './coverage/users-apis'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'api-jsonplaceholder'`, () => {
+  it(`should have as title 'users-apis'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('api-jsonplaceholder');
+    expect(app.title).toEqual('users-apis');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('api-jsonplaceholder app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('users-apis app is running!');
   });
 });
